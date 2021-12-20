d9bc4d0f32210f3b8c60c416bbbe6412d74bdc60
YEET <joshuamani@hotmail.com>
2021-12-20 15:52:59 -0500

    console.log data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,15 @@ var apiKey = "6f99554d79c2dead2ca7bf90475849c2"
 var searchBtn = document.getElementById("searchBtn");
 var cityName = document.getElementById("cityName");
 var forecastDiv = document.getElementById("forecastDiv");
+var day1 = document.getElementById("day1");
+var day2 = document.getElementById("day2");
+var day3 = document.getElementById("day3");
+var day4 = document.getElementById("day4");
+var day5 = document.getElementById("day5");
 
 https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 searchBtn.addEventListener("click", function() {
     firstApi(cityName.value);
 })
@@ -35,3 +41,4 @@ function secondApi(lat, lon) {
         }
     })
 }
+
